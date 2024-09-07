3009a5c54d0243b550215d8a9ec3b8fc73cd1611
emerson <xinayespinoza@gmail.com>
2024-09-07 10:42:38 -0400

    fix index

--- monedas.js
+++ monedas.js
@@ -1,5 +1,5 @@
 
-// const xValues = ["emerson", 60, 70, 80, 90, 100, 110, 120, 130, 140, 150];
+// const xValues = [50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150];
 // const yValues = [7, 8, 8, 9, 9, 9, 10, 11, 14, 14, 15];
 
 async function mostrarapi() {
